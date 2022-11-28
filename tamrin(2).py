d1c2141d2d5d05042d8059549c96fe4fc6e9e52c
zh-prooz79 <119279789+zh-prooz79@users.noreply.github.com>
2022-11-28 08:49:08 +0330

    Add files via upload

---
+++ tamrin(2).py
@@ -0,0 +1,48 @@
+sval='123'
+print(type(sval))
+
+# print(1+sval)
+ival=int(sval)
+type(ival)
+print(ival+1)
+
+# nsv ='hello bob'
+# niv=int(nsv)
+
+def greet ():
+    return "hello"
+
+print(greet(),"gleen")
+print(greet(),"sally")
+
+def greet(lang):
+    if lang=='es':
+        return 'hola'
+    elif lang =='fr':
+        return 'bonjour'
+    else:
+        return'hello'
+    
+print((greet('en'),'glenn'))
+
+print((greet('es'),'sally'))
+
+print((greet('fr'),'michael'))
+
+def max(inp):
+    blah
+    blah
+    for x in inp:
+     blah
+     blah
+# return 'w'
+     
+def addtwo(a,b):
+    added=a+b
+    return added
+
+x=addtwo(3,5)
+print(x)
+
+
+
---
+++ tamrin2.py
@@ -0,0 +1,36 @@
+
+def thing():
+
+    print("hello")
+
+    print("fun")
+
+thing()
+
+print("zip")
+
+thing()
+
+big=max('Hello world')
+
+print(big)
+
+tiny=min('Hello world')
+
+print(tiny)
+
+print(float(99)/100)
+
+i=42
+i=type(i)
+print(i)
+f=float(i)
+print(f)
+
+print(1+2*float(3)/4-5)
+
+
+
+
+
+
